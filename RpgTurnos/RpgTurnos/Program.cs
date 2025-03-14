55b1245e275c1da31bd9cab21d150ccb0efe0126
frodrigss <soaresrodriguesf07@icloud.com>
2025-03-14 12:34:31 -0300

    initial

---
+++ RpgTurnos/RpgTurnos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RpgTurnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem Char = new Personagem();
+            Char.criarPersonagem();
+        }
+    }
+}
